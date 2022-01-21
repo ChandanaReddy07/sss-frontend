6d3bce6b3d2fe6db147228be4205b5f139249859
ChandanaReddy07 <b219021@iiit-bh.ac.in>
2022-01-21 22:35:31 +0530

    updated

--- src/Apptest.js
+++ src/Apptest.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import App from "./App";
 import { isAuthenticated, signout } from "./helper/auth";
 // import "./App.css";
-
+//main app
 export default function Apptest() {
   useEffect(() => {
     /*==================== SHOW NAVBAR ====================*/
